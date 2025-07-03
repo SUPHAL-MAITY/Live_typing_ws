b8a88f106b2c7ce194cdbfa16680abe1554780b8
SUPHAL-MAITY <suphalrohit@gmail.com>
2025-07-03 23:05:41 +0530

    toString added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,27 @@
+import { WebSocketServer } from "ws";
+
+
+const wss=new WebSocketServer({port:8080})
+
+
+wss.on("connection",(ws)=>{
+      ws.send("user connected")
+
+      ws.on("message",(message)=>{
+
+        const msg=JSON.parse(message.toString())
+
+        if(msg.type==="msgType"){
+            wss.clients.forEach((client)=>{
+                if(client.readyState===WebSocket.OPEN){
+                    client.send(JSON.stringify({type:"msgType",payload:msg.payload.text}))
+                }
+            })
+        }
+
+
+      })
+})
 
 
 
