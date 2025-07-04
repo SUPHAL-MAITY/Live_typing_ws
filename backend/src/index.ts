f16e7f22d85e35e92159bfdd040a626438af3801
SUPHAL-MAITY <suphalrohit@gmail.com>
2025-07-04 10:38:33 +0530

    fronted socket added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ const wss=new WebSocketServer({port:8080})
 
 
 wss.on("connection",(ws)=>{
-      ws.send("user connected")
+      ws.send( JSON.stringify( { type:"connection",payload:{text:"user connected"} } ));
 
       ws.on("message",(message)=>{
 
@@ -14,7 +14,7 @@ wss.on("connection",(ws)=>{
         if(msg.type==="msgType"){
             wss.clients.forEach((client)=>{
                 if(client.readyState===WebSocket.OPEN){
-                    client.send(JSON.stringify({type:"msgType",payload:msg.payload.text}))
+                    client.send(JSON.stringify({type:"msgType", payload:{text:msg.payload.text}}))
                 }
             })
         }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,45 @@
 
+import { useState,useEffect ,useRef} from 'react'
 import './App.css'
 
 function App() {
+  const socketRef=useRef<WebSocket| null>(null)
+  const [text,setText]=useState<string | null >("")
+
+  useEffect(()=>{
+    socketRef.current=new WebSocket("ws://localhost:8080")
+
+   
+
+    socketRef.current.onmessage=(event)=>{
+      const msg=JSON.parse(event.data)
+      console.log(msg)
+
+      if(msg.type=="msgType"){
+        setText(msg.payload.text)
+      }
+
+    }
+
+  },[])
+
+
+  const handleChange=(e:React.ChangeEvent<HTMLTextAreaElement>)=>{
+    setText(e.target.value)
+    socketRef.current?.send(JSON.stringify({ type:"msgType",payload:{ text:e.target.value}}))
+
+  }
+   
  
 
   return (
     <>
+    <div className=" flex flex-col items-center justify-center mt-4">
+         <h1 className='text-3xl font-serif '>Live Typing </h1>
+         <textarea value={text?.toString()} onChange={handleChange} className='border border-gray-900 w-96 mt-4 h-96 p-2' />
+
+    </div>
+    
      
     </>
   )
